31034641d54b61621b04ab8436e2dbe0eaf77c42
Rogarsu2014 <rogarsu2014@gmail.com>
2023-07-21 13:33:18 +0200

    funciona (algo)

--- Cart-Pole.py
+++ Cart-Pole.py
@@ -1,20 +1,40 @@
 import gymnasium as gym
 from Qalgo import *
 import numpy as np
+import os
+import random
 
 env = gym.make('CartPole-v1', render_mode="human")
-observation, info = env.reset(seed=42)
-print(env.observation_space)
+#observation, info = env.reset(seed=42)
 
-num_bins = [10] * env.observation_space.shape.count()
+alpha = 0.15
+gamma = 0.95
 
+num_explorations = 10000
+num_exploitations = 1000
 
+#num_bins = [10] * env.observation_space.shape[0]
+num_bins = [20,10,20,10]
 
+state_bins = get_state_bins(env, num_bins)
 
-for _ in range(1000):
+observation, info = env.reset()
+
+initial_state = discretize_state(observation, state_bins)
+
+# Initialize the Q-table with zeros (numbins and numactions)
+q_table = np.zeros((*num_bins, env.action_space.n))
+
+if not os.path.exists("CartPole_q_table.npy"):
+    explore(num_explorations, alpha, gamma, q_table, state_bins, env, initial_state)
+
+exploit(num_exploitations, env, state_bins)
+
+'''for _ in range(1000):
    action = env.action_space.sample()  # this is where you would insert your policy
    observation, reward, terminated, truncated, info = env.step(action)
 
    if terminated or truncated:
       observation, info = env.reset()
+'''
 env.close()
--- Qalgo.py
+++ Qalgo.py
@@ -1,2 +1,86 @@
-def calcularQ(actualQ, alpha, reward, gamma, max_next_state):
+import numpy as np
+import random
+
+def calculateQ(actualQ, alpha, reward, gamma, max_next_state):
     return (1 - alpha) * actualQ + alpha * (reward + gamma * max_next_state)
+
+
+def get_state_bins(env, num_bins):
+    state_bins = []
+    for i in range(env.observation_space.shape[0]):
+        state_low, state_high = env.observation_space.low[i], env.observation_space.high[i]
+        state_bins.append(np.linspace(state_low, state_high, num_bins[i] + 1)[1:-1])
+    return state_bins
+
+def discretize_state(state, state_bins):
+    # Discretize each state variable based on the bin values
+    discrete_state = []
+    for i in range(len(state)):
+        discrete_state.append(np.digitize(state[i], state_bins[i]))
+    return tuple(discrete_state)
+
+
+def explore(num_cycles, alpha, gamma, q_table, state_bins, env, initial_state):
+
+    discrete_state = initial_state
+
+    learning_curve = (num_cycles-num_cycles/3)
+
+    for i in range(num_cycles):
+
+        if random.randint(i, num_cycles) < learning_curve:
+            action = env.action_space.sample() #accion aleatoria
+        else:
+            action = np.argmax(q_table[discrete_state])#mejor accion
+
+        q_value = q_table[discrete_state + (action,)]
+
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        next_discrete_state = discretize_state(observation, state_bins)
+        next_action = np.argmax(q_table[next_discrete_state])
+        new_q_value = calculateQ(q_value, alpha, reward, gamma, q_table[next_discrete_state + (next_action,)])
+
+        # Update the Q-table with the new Q-value
+        q_table[discrete_state + (action,)] = new_q_value
+
+        #pasar al siguiente
+        discrete_state = next_discrete_state
+
+        if terminated or truncated:
+            observation, info = env.reset()
+            initial_state = discretize_state(observation, state_bins)
+            discrete_state = initial_state
+
+    print("End Exploration")
+    np.save('CartPole_q_table.npy', q_table)
+
+def exploit(num_cycles, env, state_bins):
+
+    count = 0
+    max_count = 0
+
+    q_table = np.load('CartPole_q_table.npy')
+    observation, info = env.reset()
+    initial_state = discretize_state(observation, state_bins)
+    discrete_state = initial_state
+
+    for _ in range(num_cycles):
+        action = np.argmax(q_table[discrete_state])
+        observation, reward, terminated, truncated, info = env.step(action)
+        discrete_state = discretize_state(observation, state_bins)
+
+        count += 1
+
+        if terminated or truncated:
+            observation, info = env.reset()
+            initial_state = discretize_state(observation, state_bins)
+            discrete_state = initial_state
+
+            print(count)
+            if count > max_count:
+                max_count = count
+            count = 0
+
+    print("End Exploitation")
+    print(max_count)
