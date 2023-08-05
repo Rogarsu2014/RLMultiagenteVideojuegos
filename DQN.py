746deb95fb84b821b66f4291b0c457ca5045fcad
Rogarsu2014 <rogarsu2014@gmail.com>
2023-08-05 19:15:37 +0200

    Bonito

--- DQN.py
+++ DQN.py
@@ -4,17 +4,19 @@ from libs.pyRIL.RL_Agent.base.utils import agent_saver, history_utils
 
 import gymnasium as gym
 
-environment = gym.make("CartPole-v1", render_mode="human")
+environment = gym.make("MountainCar-v0")#, render_mode="human"
+
+name = "MountainCar-v0"
 
 agent = dqn_agent.Agent(learning_rate=1e-3, batch_size=128, epsilon=0.4, epsilon_decay=0.999, epsilon_min=0.15)
 
 problem = dqn_problem.DQNProblem(environment, agent)
 
-problem.solve(30, render=True, verbose=1)
+problem.solve(30, verbose=1)
 
 problem.test(n_iter=10, verbose=1)
 
 hist = problem.get_histogram_metrics()
 history_utils.plot_reward_hist(hist, n_moving_average=10)
 
-agent_saver.save(agent, 'agent_dqn_lunar.json')
+agent_saver.save(agent, name + '_agent.json')
