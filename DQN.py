998660d9dffa5f748f55864edf69a50a11612a98
Rogarsu2014 <rogarsu2014@gmail.com>
2023-09-18 14:10:42 +0200

    Volvimos

--- DQN.py
+++ DQN.py
@@ -3,20 +3,40 @@ from libs.pyRIL.RL_Problem.base.ValueBased import dqn_problem
 from libs.pyRIL.RL_Agent.base.utils import agent_saver, history_utils
 
 import gymnasium as gym
+import os
+import tensorflow as tf
 
-environment = gym.make("MountainCar-v0")#, render_mode="human"
+# Get the list of available GPU devices
+gpu_devices = tf.config.list_physical_devices('GPU')
 
-name = "MountainCar-v0"
+if gpu_devices:
+    print("TensorFlow is using GPU.")
+else:
+    print("TensorFlow is using CPU.")
+
+
+environment = gym.make("CartPole-v1")#, render_mode="human"
+
+name = "CartPole-v1"
+
+show_results = False
 
 agent = dqn_agent.Agent(learning_rate=1e-3, batch_size=128, epsilon=0.4, epsilon_decay=0.999, epsilon_min=0.15)
 
+if show_results:
+    if os.path.exists(name + '_agent.json'):
+        agent = agent_saver.load(name + '_agent.json', agent)
+    else:
+        show_results = False
+
 problem = dqn_problem.DQNProblem(environment, agent)
 
-problem.solve(30, verbose=1)
+if not show_results:
+    problem.solve(30, verbose=1)
+    hist = problem.get_histogram_metrics()
+    history_utils.plot_reward_hist(hist, n_moving_average=10)
 
 problem.test(n_iter=10, verbose=1)
 
-hist = problem.get_histogram_metrics()
-history_utils.plot_reward_hist(hist, n_moving_average=10)
-
-agent_saver.save(agent, name + '_agent.json')
+if not show_results:
+    agent_saver.save(agent, name + '_agent.json')
